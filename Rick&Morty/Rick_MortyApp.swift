af4037cd1d7939da3848c32d9ddc7e79ee104cc5
Apple <apple@Apples-MacBook-Pro-2.local>
2026-05-12 22:36:16 +0530

    Initial commit: added .gitignore

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Apple on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
